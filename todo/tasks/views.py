8032dcf8c532b88b227e9b4b4630a03a7b254e8e
Avat <avatolzhobaeva@gmail.com>
2021-08-12 20:12:28 +0600

    set up CRUD for task

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 class TaskAPIView(APIView):
     
@@ -24,3 +25,29 @@ class TaskAPIView(APIView):
             return Response(serializer.data)
 
         return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskDetailAPIView(APIView):
+
+    def get(self, request, id):
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskSerializer(instance=task)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskSerializer(instance=task, data=request.data)
+
+        if serializer.is_valid():
+            body = serializer.validated_data.get('body')
+            est = serializer.validated_data.get('estimated_finish_time')
+            task.body = body
+            task.estimated_finish_time = est
+            task.save()
+            return Response(serializer.data)
+        return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        task = get_object_or_404(Task, id=id)
+        task.delete()
+        return Response({'detail: success'}, status=status.HTTP_204_NO_CONTENT)
